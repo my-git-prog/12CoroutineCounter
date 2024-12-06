123f01273b6d1b7c428ba8401f2687a25e12f033
my-git-prog <pavel.serpukhov@gmail.com>
2024-12-06 15:22:43 +0300

    first uploading

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private float _delay = 0.5f;
+    [SerializeField] private int _increment = 1;
+    [SerializeField] private int _startValue = 0;
+
+    private int _currentValue;
+    private bool _isRunned = false;
+
+    private void Start()
+    {
+        _currentValue = _startValue;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ChangeCounterState();
+        }
+    }
+
+    private void ChangeCounterState()
+    {
+        _isRunned = !_isRunned;
+        
+        if(_isRunned)
+        {
+            StartCoroutine(IncrementCounter());
+        }
+    }
+
+    private IEnumerator IncrementCounter()
+    {
+        Debug.Log("Start Counter");
+
+        var wait = new WaitForSeconds(_delay);
+
+        while (_isRunned)
+        {
+            _currentValue += _increment;
+            Debug.Log(_currentValue);
+            
+            yield return wait;
+        }
+
+        Debug.Log("Stop Counter");
+        
+        yield break;
+    }
+}
